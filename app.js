d94c2ad3baa81d707a6bc19aa4e78f303784d4eb
Vinh Nguyễn <vinhnguyen50501@gmail.com>
2023-07-16 21:46:47 +0700

    setup project

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
-const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
 const { spawnSync } = require("child_process");
 const os = require("os");
 const cron = require("node-cron");
-const mongoose = require('mongoose')
-const isAuthorized = require('./isAuthorized');
+const session = require("express-session");
+const mongoose = require("mongoose");
+const isAuthorized = require("./isAuthorized");
 const authRoute = require("./routes/auth");
 const adminRoute = require("./routes/adminRoute");
 const managerRoute = require("./routes/managerRoute");
@@ -22,22 +23,29 @@ mongoose
   )
   .then(() => console.log("Connected!"));
 
-
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-app.use(logger('dev'));
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', (_, res) => res.render('index'))
+app.use(express.static(path.join(__dirname, "public")));
+app.use(
+  session({
+    secret: "your_secret_key",
+    resave: false,
+    saveUninitialized: false,
+  })
+);
 app.use("/test", (_, res) => {
   res.status(200).send("working");
 });
+app.get("/", (_, res) => {
+  return res.render("index");
+});
 app.use("/auth", authRoute);
 app.use("/admin", adminRoute);
-app.use("/manager" , managerRoute);
+app.use("/manager", managerRoute);
 app.use("/staff", staffRoute);
 
 app.listen("3000", () => {
@@ -51,7 +59,5 @@ cron.schedule("10 * * * *", () => {
 
   pingHost();
 });
-
-
 
 module.exports = app;
