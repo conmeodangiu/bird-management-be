2bc47947380e5da8fc516deda7ead0179a1083e7
iuuukhueeee <nguyenducthien9@gmail.com>
2023-07-16 21:54:31 +0700

    added crud staff route

--- routes/staffRoute.js
+++ routes/staffRoute.js
@@ -1,4 +1,34 @@
 const express = require("express");
 const router = express.Router();
+const Event = require("../schema/event");
+
+router.get("/", (req, res) => {
+  Event.find({}).then((event) => {
+    //
+    return res.json(event);
+  });
+});
+
+router.post('/create', (req,res) => {
+  const {body} = req.body;
+  const event = new Event({
+    playerOne: body.playerOne,
+    playerTwo: body.playerTwo,
+    time: body.time,
+    titleMatch: body.titleMatch,
+  })
+  Event.create(event).then(() => {
+    //
+    res.json('create event success')
+  })
+})
+
+router.get("/delete:/id", (req, res) => {
+  const id = req.params.id;
+  Event.findByIdAndDelete({ _id: id }).then(() => {
+    //
+    return res.json("delete successfully");
+  });
+});
 
 module.exports = router;
