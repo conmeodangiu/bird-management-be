f6380b7beb37160cbe0eaa9c7684d05230c04d91
dieuanh14 <anhvtdse161144@fpt.edu.vn>
2023-07-16 21:34:36 +0700

    'login/register'

--- routes/auth.js
+++ routes/auth.js
@@ -1,32 +1,64 @@
 const express = require("express");
 const Users = require("../schema/user");
-const router = express.Router();
+const bcrypt = require("bcrypt");
 
+const router = express.Router();
+router.get("/login", (req, res) => {
+  return res.render("login");
+});
+router.get("/home", (req, res) => {
+  const username = req.session.username; 
+  res.render("home", { username }); 
+});
 router.post("/login", (req, res) => {
   const { body } = req;
-  Users.findOne({ username: body.username, password: body.password })
+  Users.findOne({ username: body.username })
     .then((found) => {
       if (found) {
-        res.status(200).json("login success");
-      } else {  
-        res.status(400).json('login failed');
+        bcrypt.compare(body.password, found.password, (err, result) => {
+          if (err) {
+            res.status(500).json("error comparing passwords");
+          } else if (result) {
+            req.session.username = found.username; 
+            res.redirect("/auth/home");
+          } else {
+            res.status(400).json("login failed");
+          }
+        });
+      } else {
+        res.status(400).json("login failed");
       }
     })
     .catch((err) => console.log(err));
 });
-
+router.get("/register", (req, res) => {
+  return res.render("register");
+});
 router.post("/register", (req, res) => {
   const { body } = req;
-  console.log(body);
-  Users.findOne({ username: body.username, password: body.password })
+  Users.findOne({ username: body.username })
     .exec()
     .then((result) => {
       if (result) {
         res.status(400).json("duplicate user");
       } else {
-        const newUser = Users(body);
-        Users.create(newUser).then(() => {
-          res.status(200).json("create successfully");
+        bcrypt.hash(body.password, 10, (err, hashedPassword) => {
+          if (err) {
+            res.status(500).json("error hashing password");
+          } else {
+            const newUser = new Users({
+              username: body.username,
+              password: hashedPassword,
+            });
+            newUser
+              .save()
+              .then(() => {
+                res.redirect("/auth/login");
+              })
+              .catch((err) => {
+                res.status(500).json("error creating user");
+              });
+          }
         });
       }
     });
