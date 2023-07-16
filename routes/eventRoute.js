58b6c249f4d893b126445c3c973ecd5731727df9
iuuukhueeee <nguyenducthien9@gmail.com>
2023-07-17 01:34:14 +0700

    allow user to create event

--- routes/eventRoute.js
+++ routes/eventRoute.js
@@ -1,14 +1,14 @@
 const express = require("express");
 const router = express.Router();
-const Event = require("../schema/event");
+const Events = require("../schema/event");
 
 // create new event
 router.post("/create-new-event", (req, res) => {
   const user = req.user;
   const { body } = req;
-  const event = {...body, playerOne: user._id}
+  const event = { ...body, playerOne: user._id };
   // {"playerOne":"khue","playerTwo":"dieuanh","date":"2023-07-07T02:00","titleMatch":"hello ba"}
-  Event.create(event).then(() => {
+  Events.create(event).then(() => {
     return res.redirect("/auth/home");
   });
 });
