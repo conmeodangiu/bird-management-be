5866d7461fa5ad5bbc7b079433f2db83dd9fe922
iuuukhueeee <nguyenducthien9@gmail.com>
2023-07-17 02:07:35 +0700

    Modify authorized middleware for blog route

--- isAuthorized.js
+++ isAuthorized.js
@@ -1,28 +1,46 @@
 const jwt = require("jsonwebtoken");
+const User = require("./schema/user");
 require("dotenv").config();
 
 const isAuthorized = (req, res, next) => {
   const token = req.cookies.token;
-  if (!token) return res.sendStatus(401);
+  const { baseUrl, method } = req;
+  if (baseUrl === "/blog" && method === "GET") {
+    const path = req.path.split("/");
+    console.log(path)
+    if (
+      path.includes("edit") ||
+      path.includes("create") ||
+      path.includes("update") ||
+      path.includes("remove")
+    ) {
+      return res.status(404).render("404");
+    }
+    const user = new User({
+      username: null,
+      password: null,
+      fullName: null,
+      role: "GUEST",
+    });
+    req.user = user;
+    return next();
+  }
+  if (!token) return res.status(404).render("404");
   const secret = process.env.secret;
 
   jwt.verify(token, secret, (err, user) => {
     if (err) {
       console.log(err);
-      return res.sendStatus(401);
+      return res.render("404");
     }
-    const { baseUrl } = req;
-    console.log(baseUrl);
+
     if (baseUrl === "/admin" && user.role === "ADMIN") {
       req.user = user;
       return next();
     } else if (baseUrl === "/staff" && user.role === "STAFF") {
       req.user = user;
       return next();
-    } else if (
-      baseUrl === "/blog" &&
-      (user.role === "MEMBER" || user.role === "ADMIN" || user.role === "STAFF")
-    ) {
+    } else if (baseUrl === "/blog") {
       req.user = user;
       return next();
     } else if (
@@ -32,10 +50,8 @@ const isAuthorized = (req, res, next) => {
       req.user = user;
       return next();
     } else {
-      res.sendStatus(403);
+      return res.status(404).render("404");
     }
-
-    return res.redirect("/");
   });
 };
 
