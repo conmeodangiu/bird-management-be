439b653e6056015a8d18a4293187f760642c7478
dieuanh14 <vuongtrandieuanh14@gmail.com>
2023-07-21 21:02:40 +0700

    ctrl-z eventRoute

--- routes/eventRoute.js
+++ routes/eventRoute.js
@@ -3,34 +3,86 @@ const router = express.Router();
 const Events = require("../schema/event");
 const isAuthorized = require("../isAuthorized");
 const Users = require("../schema/user");
-
-// create new event
 router.get("/create-event", (req, res) => {
   return res.render("create-event");
 });
 
-router.post("/create-event", async (req, res) => {
-  const { titleMatch, description, member, field, startDate, endDate, status } = req.body;
+// create new event
+// router.post("/create-new-event", isAuthorized(["MEMBER"]), (req, res) => {
+//   const user = req.user;
+//   const { body } = req;
+//   const event = { ...body, playerOne: user._id };
+//   // {"playerOne":"khue","playerTwo":"dieuanh","date":"2023-07-07T02:00","titleMatch":"hello ba"}
+//   Events.create(event).then(() => {
+//     return res.redirect("/auth/home");
+//   });
+// });
 
-  try {
-    const newEvent = new Events({
-      titleMatch,
-      description,
-      participants: {
-        member,
-        field
-      },
-      startDate,
-      endDate,
-      status
-    });
+// router.get("/matches", isAuthorized(["STAFF"]), (_, res) => {
+//   Events.find({})
+//     .populate("playerOne")
+//     .populate("playerTwo")
+//     .exec()
+//     .then((matches) => res.render("matches", { matches }))
+//     .catch((err) => console.log(err));
+// });
 
-    await newEvent.save();
-    return res.redirect("/event/create-event");
-  } catch (err) {
-    console.error(err);
-    return res.redirect("/404");
-  }
-});
+// router.get("/start/:id", (req, res) => {
+//   const id = req.params.id;
+//   Events.findByIdAndUpdate({ _id: id }, { status: "STARTED" })
+//     .then(() => {
+//       Events.find({})
+//         .populate("playerOne")
+//         .populate("playerTwo")
+//         .then((matches) => {
+//           return res.render("matches", { matches });
+//         })
+//         .catch((err) => console.log(err));
+//     })
+//     .catch((err) => console.log(err));
+// });
+
+// router.post("/set-result", async (req, res) => {
+//   const { body } = req;
+//   try {
+//     await Events.findByIdAndUpdate({ _id: body.id }, { status: body.result });
+//     if (body.result === "VICTORY") {
+//       await Users.findByIdAndUpdate(
+//         { _id: body.playerOneId },
+//         { $inc: { victory: 1 } }
+//       );
+//       await Users.findByIdAndUpdate(
+//         { _id: body.playerTwoId },
+//         { $inc: { lose: 1 } }
+//       );
+//     } else if (body.result === "DRAW") {
+//       await Users.findByIdAndUpdate(
+//         { _id: body.playerOneId },
+//         { $inc: { draw: 1 } }
+//       );
+//       await Users.findByIdAndUpdate(
+//         { _id: body.playerTwoId },
+//         { $inc: { draw: 1 } }
+//       );
+//     } else if (body.result === "LOSE") {
+//       await Users.findByIdAndUpdate(
+//         { _id: body.playerOneId },
+//         { $inc: { lose: 1 } }
+//       );
+//       await Users.findByIdAndUpdate(
+//         { _id: body.playerTwoId },
+//         { $inc: { victory: 1 } }
+//       );
+//     }
+
+//     const matches = await Events.find({})
+//       .populate("playerOne")
+//       .populate("playerTwo")
+//       .exec();
+//     return res.render("matches", { matches });
+//   } catch (err) {
+//     console.log(err);
+//   }
+// });
 
 module.exports = router;
