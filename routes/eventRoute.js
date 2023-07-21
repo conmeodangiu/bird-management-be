082c112a9404ecadb0ac70ca5312ba168ace22ec
Vinh Nguyễn <vinhnguyen50501@gmail.com>
2023-07-21 14:56:25 +0700

    change path event page

--- routes/eventRoute.js
+++ routes/eventRoute.js
@@ -5,7 +5,7 @@ const isAuthorized = require("../isAuthorized");
 const Users = require("../schema/user");
 
 // create new event
-router.get("/create-event", (req, res) => {
+router.get("/", (req, res) => {
   return res.render("create-event");
 });
 
@@ -32,5 +32,42 @@ router.post("/create-event", async (req, res) => {
     return res.redirect("/404");
   }
 });
+
+router.post("/judge-event", (req, res) => {
+  const competitionId = req.params; // Replace with the actual competition ID
+  const memberId = 'memberId'; // Replace with the actual member ID
+  const staffId = req.params; // Replace with the actual staff ID
+  const gradingFields = {
+    field1: 8,
+    field2: 'Great job!',
+  };
+
+  // Find the competition document and add the grading detail for the specific participant
+  Events.findOneAndUpdate(
+    {
+      _id: competitionId,
+      'participants.member': memberId, // Match the competition and the specific participant
+    },
+    {
+      $push: {
+        'participants.$.gradingDetails': {
+          staff: staffId,
+          date: new Date(),
+          gradingFields: gradingFields,
+        },
+      },
+    },
+    { new: true } // Set { new: true } to return the updated document
+  )
+    .exec((err, updatedCompetition) => {
+      if (err) {
+        console.error('Error adding grading detail:', err);
+      } else if (!updatedCompetition) {
+        console.log('Competition or participant not found.');
+      } else {
+        return res.redirect("/event")
+      }
+    });
+})
 
 module.exports = router;
